fbdf13d7ba21c9f1daf487a8efc1c7b59231f157
EC2 Default User <ec2-user@ip-172-31-0-61.ap-northeast-1.compute.internal>
2022-11-21 10:22:59 +0000

    [Bookers2]-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,14 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "4a572185eb434738bbb1137059b6c7ac.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 end
